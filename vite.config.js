ff11efd42400df4bb8071226bfc5b7aa05d0a8d3
MatteoGabriele <m.gabriele.dev@gmail.com>
2025-04-08 22:33:58 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,44 @@
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.ts"),
+      name: "Kashyyyk",
+      formats: ["es"],
+      fileName: "kashyyyk",
+    },
+    rollupOptions: {
+      external: ["vue", "vue-router"],
+      output: {
+        exports: "named",
+        globals: {
+          vue: "Vue",
+          "vue-router": "VueRouter"
+        },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+    }),
+  ],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    mockReset: true,
+    coverage: {
+      reporter: ['text', 'html'],
+    }
+  },
+});
